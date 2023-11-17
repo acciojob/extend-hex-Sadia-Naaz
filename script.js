71e57e5475ded890416b84c9e8a03b01a33c30ac
Sadia Naaz <139625425+Sadia-Naaz@users.noreply.github.com>
2023-11-17 11:53:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const extendHex = (shortHex) => {
   // write your code here
+	const HexWithoutHash = shortHex.replace(/^#/,'');
+	const red = HexWithoutHash[0];
+	const green = HexWithoutHash[1];
+	const blue = HexWithoutHash[2];
+	const fullHex = `${red}${red}${green}${green}${blue}${blue}`;
+	return `${fullHex}`
+		
+	;
+	
 };
+    
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
